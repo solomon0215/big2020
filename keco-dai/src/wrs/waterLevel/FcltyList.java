c9a11acc52c915bcd7853066c466ed9bc52d1df1
solom <solom@DESKTOP-UPQQ4PE>
2020-07-09 09:18:09 +0900

    20200709 오타 수정 -민재

--- keco-dai/src/wrs/waterLevel/FcltyList.java
+++ keco-dai/src/wrs/waterLevel/FcltyList.java
@@ -37,7 +37,7 @@ public class FcltyList {
 
 					// step 1.파일의 작성
 //					File file = new File("../../../../test.dat");
-					File file = new File(JsonParser.getProperty("wfile_path") + "WRS/TIF_WRS_06.dat");
+					File file = new File(JsonParser.getProperty("file_path") + "WRS/TIF_WRS_06.dat");
 
 					// step 2. 전체 데이터 숫자 파악을 위해 페이지 수 1으로 파싱
 					//String json = "";
